81b3102f8bf9ada1077a62e770ccd251f536bf7d
Stanley Lau <stanleyyylau@gmail.com>
2016-08-08 11:51:45 +0000

    initial app

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+
+
+app.get('/', function (req, res) {
+    res.send("Hello You");
+});
+  
+  
+  
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
